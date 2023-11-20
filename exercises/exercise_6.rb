9de1f28806232ee443862f3c45d07c4af1d1b43c
lunamoonmoon <5862lunamoon@gmail.com>
2023-11-19 16:01:51 -0800

    completed all exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,18 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Donald", last_name: "Duck", hourly_rate: 120)
+@store1.employees.create(first_name: "Daisy", last_name: "Duck", hourly_rate: 100)
+@store2.employees.create(first_name: "Minnie", last_name: "Mouse", hourly_rate: 60)
+@store2.employees.create(first_name: "Mickey", last_name: "Mouse", hourly_rate: 60)
+@store2.employees.create(first_name: "Princess", last_name: "Zelda", hourly_rate: 20)
+@store2.employees.create(first_name: "Link", last_name: "Zelda", hourly_rate: 30)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,20 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Store
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: { only_integer: true, greater_than: 20, less_than: 201 }
+  validates :store, presence: true
+end
+
+puts "Enter your store name:"
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+new_store.errors.full_messages.each { |msg| puts msg }
